b96d8ba1f60a1c7d56a938c7ff6e497e8a05b814
ricardoayala2510 <143136224+ricardoayala2510@users.noreply.github.com>
2025-02-03 14:45:49 -0600

    Update binary search code.cpp

--- notas/binary search code.cpp
+++ notas/binary search code.cpp
@@ -1,1 +1,31 @@
-#include 
+#include <iostream>
+
+
+bool in_array(int *a, int){
+    
+}
+
+
+int main() {
+	int size = 100;
+	int *nums = new int[size];
+	
+	bool unique;
+	int r;
+
+	for(int i = 0; i < size; i++) {
+
+		unique = true;
+		while(unique) {
+			int r = rand() % 10000;
+			for ( int j = 0; j <= i; j++) {
+				if(nums[j] == r) {
+					unique = false;
+					break;
+				}
+			}
+		}
+		nums[i]=r;
+
+	}
+}
